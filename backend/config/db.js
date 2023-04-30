3d27cf2e87b3999ae0ad83ab7b9d731d85a0fee6
RamKashyap2050 <ramkashyap2050@gmail.com>
2023-04-30 12:44:25 -0400

    Deploy attempt

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
-const Grid = require('gridfs-stream');
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        const gfs = Grid(conn.connection.db, mongoose.mongo);
-        gfs.collection('Users')
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
     }catch(error){
         console.log(error)
