e9cc2d61bad305c8bf4713bbf4975eadea1e3e44
RamKashyap2050 <ramkashyap2050@gmail.com>
2023-11-27 16:33:47 -0500

    Improvised Community

--- backend/models/CommunityContentModel.js
+++ backend/models/CommunityContentModel.js
@@ -1,26 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const communitySchema = mongoose.Schema({
-  Community_ID: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'Community',
+const communitySchema = mongoose.Schema(
+  {
+    Community_ID: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Community",
+    },
+    User_ID:{
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref:"Users"
+    },
+    Post_Image: {
+      type: String,
+    },
+    Content: {
+      type: String,
+    },
   },
-  Title: {
-    type: String,
-    required: true
-  },
-  Post_Image: {
-    type: String
-  },
-  Content:{
-    type: String
+  {
+    collection: "CommunityContent",
+    timestamp: true,
   }
+);
 
-},
-{
-  collection: 'CommunityContent',
-  timestamp: true
-});
-
-module.exports = mongoose.model('CommunityContent', communitySchema);
+module.exports = mongoose.model("CommunityContent", communitySchema);
--- frontend/src/pages/CreateCommunityContent.jsx
+++ frontend/src/pages/CreateCommunityContent.jsx
@@ -6,11 +6,17 @@ import Footer from "../components/Footer";
 import { useSelector } from "react-redux";
 import { FaImage } from "react-icons/fa";
 import "../styles/Feedpage.css";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+
 const CreateCommunityContent = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
   const [communityData, setCommunityData] = useState("");
+  const [show, setShow] = useState(false);
+  const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
     console.log("useEffect triggered with user:", user);
@@ -33,6 +39,27 @@ const CreateCommunityContent = () => {
       });
   }, [id]);
   console.log(user.user_name);
+
+  const onAddMembers = () => {
+    setShow(true);
+  };
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/loginuser");
+    } else {
+      axios
+        .get(`/Follow/getfollowersforuser/${user._id}`)
+        .then((response) => {
+          const fetchedFollowers = response.data.followers;
+          setFollowers(fetchedFollowers);
+          console.log(followers);
+        })
+        .catch((error) => {
+          console.error("Error fetching followers:", error);
+        });
+    }
+  }, [user, navigate]);
 
   const onChange = (e) => {
     // if (e.target.type === "file") {
@@ -78,7 +105,23 @@ const CreateCommunityContent = () => {
     // }
   };
 
-  console.log("Community Data selected", communityData);
+  const handleAddMembersSubmit = (event) => {
+    // Add your logic for submitting members here
+    event.preventDefault();
+    // Your logic for adding members to the community
+    setShow(false);
+  };
+
+  const handleAddMembersClick = () => {
+    setShow(true);
+    console.log("Clicked");
+  };
+
+  const handleModalClose = () => {
+    setShow(false);
+  };
+
+  console.log(show);
   return (
     <div>
       <HeaderforUser />
@@ -95,10 +138,18 @@ const CreateCommunityContent = () => {
         </div>
         <br />
         <h3>{communityData.Community_Name}</h3>
-        <div style={{ display: "flex", justifyContent: "space-around", textAlign:"center", maxWidth:"12%", margin:"10px auto" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            textAlign: "center",
+            maxWidth: "12%",
+            margin: "10px auto",
+          }}
+        >
           <img src={user.image} alt="" className="Commentprofilephoto" />
 
-          <p style={{fontWeight:"bold"}}>{user?.user_name}</p>
+          <p style={{ fontWeight: "bold" }}>{user?.user_name}</p>
         </div>
         <p>{communityData.Community_Description}</p>
       </div>
@@ -163,12 +214,63 @@ const CreateCommunityContent = () => {
           Submit
         </button>
       </form>
+      {show ? (
+        <Modal
+          show={show}
+          onHide={handleModalClose}
+          aria-labelledby="contained-modal-title-vcenter"
+          style={{ opacity: 1, marginTop: "15rem" }}
+          centered
+        >
+          <Modal.Header>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Add Members
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p style={{fontWeight:"bold"}}>You can add people from your followers to Community: </p><br />
+            <div style={{overflowY:"scroll"}}>
+            {followers.map((follower) => (
+              <div key={follower.id} style={{display:"flex", justifyContent:"space-between", marginBottom:"2rem"}}>
+               <div style={{display:"flex"}}>
+               <img src={follower.followed_by_ID.image}  className="Commentprofilephoto" />&nbsp;&nbsp;
+                <p>{follower.followed_by_ID.user_name}</p>
+               </div>
+                <button className="btn btn-secondary">Join</button>
+              </div>
+            ))}
+            </div>
+            <Button
+              type="submit"
+              variant="primary"
+              onSubmit={handleAddMembersSubmit}
+            >
+              Add Members
+            </Button>
+          </Modal.Body>
+        </Modal>
+      ) : null}
       <br />
       <br />
-      <div style={{ display: "flex", justifyContent: "space-between", margin:"auto", maxWidth:"80%" }}>
-        <button className="btn btn-primary" style={{width:"100%"}}>Add Memebers</button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          margin: "auto",
+          maxWidth: "80%",
+        }}
+      >
+        <button
+          className="btn btn-primary"
+          style={{ width: "100%" }}
+          onClick={handleAddMembersClick}
+        >
+          Add Memebers
+        </button>
         &nbsp;&nbsp;
-        <button className="btn btn-secondary" style={{width:"100%"}}>Edit Members</button>
+        <button className="btn btn-secondary" style={{ width: "100%" }}>
+          Edit Members
+        </button>
       </div>
       <Footer />
     </div>
